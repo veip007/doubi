6d8f65acc73db2f34f50c78f733e6577615cfbdf
veip007 <veip007@vip.qq.com>
2018-11-20 17:28:42 +0800

    libsodium 一键安装脚本 (Chacha20 / Chacha20-ietf 等)

--- libsodium.sh
+++ libsodium.sh
@@ -6,7 +6,7 @@ export PATH
 #	System Required: CentOS/Debian/Ubuntu
 #	Description: Libsodium Install
 #	Version: 1.0.0
-#	Author: Toyo
+#	Author: Toyo 
 #	Blog: https://doub.io/shell-jc6/
 #=================================================
 
