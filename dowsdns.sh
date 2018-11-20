9736c55fbe48ae8556fd6b2c9604778bea40ced0
veip007 <veip007@vip.qq.com>
2018-11-20 17:23:03 +0800

    快速自建DNS服务器以 科学上网+屏蔽广告 —— dowsDNS 一键脚本

--- dowsdns.sh
+++ dowsdns.sh
@@ -6,7 +6,7 @@ export PATH
 #	System Required: CentOS/Debian/Ubuntu
 #	Description: dowsDNS
 #	Version: 1.0.10
-#	Author: Toyo
+#	Author: Toyo 
 #	Blog: https://doub.io/dowsdns-jc3/
 #=================================================
 
