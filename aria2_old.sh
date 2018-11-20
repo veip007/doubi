0aadb5c768643c46cd841b972bedbf38ee433fb4
veip007 <veip007@vip.qq.com>
2018-11-20 17:20:13 +0800

    保留 Aria2 旧脚本。

--- aria2_old.sh
+++ aria2_old.sh
@@ -6,7 +6,7 @@ export PATH
 #	System Required: CentOS/Debian/Ubuntu
 #	Description: Aria2
 #	Version: 1.0.4
-#	Author: Toyo
+#	Author: Toyo 
 #	Blog: https://doub.io/shell-jc4/
 #=================================================
 sh_ver="1.0.4"
