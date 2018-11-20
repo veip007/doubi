4ca68499f7b47cb8ab823f184035503bbbe62b8d
veip007 <veip007@vip.qq.com>
2018-11-20 17:18:46 +0800

    多服务器 云探针、云监控 —— ServerStatus 一键管理脚本

--- status.sh
+++ status.sh
@@ -325,8 +325,8 @@ Set_password(){
 	else
 		echo -e "请输入 ServerStatus 服务端中对应配置的密码[password]（字母/数字）"
 	fi
-	read -e -p "(默认: doub.io):" password_s
-	[[ -z "$password_s" ]] && password_s="doub.io"
+	read -e -p "(默认: 1024sg.cf):" password_s
+	[[ -z "$password_s" ]] && password_s="1024sg.cf"
 	echo && echo "	================================================"
 	echo -e "	密码[password]: ${Red_background_prefix} ${password_s} ${Font_color_suffix}"
 	echo "	================================================" && echo
