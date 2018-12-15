d0bd70c5088555189b237f87375b1d266fa149d6
veip007 <12257917+veip007@users.noreply.github.com>
2018-12-15 22:15:32 +0800

    SSR多用户一键脚本 支持流量限制等 V2.0.0

--- ssrmu.sh
+++ ssrmu.sh
@@ -5,12 +5,12 @@ export PATH
 #=================================================
 #	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
 #	Description: Install the ShadowsocksR mudbjson server
-#	Version: 2.0.0
+#	Version: 2.0.0 
 #	Author: Toyo
 #	Blog: https://doub.io/ss-jc60/
 #=================================================
 
-sh_ver="1.0.26"
+sh_ver="2.0.0"
 filepath=$(cd "$(dirname "$0")"; pwd)
 file=$(echo -e "${filepath}"|awk -F "$0" '{print $1}')
 ssr_folder="/usr/local/shadowsocksr"
